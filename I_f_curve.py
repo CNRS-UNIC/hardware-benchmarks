d66c36a69df690c112ee855ff9947843094f9a7f
JoelChavas <joel.chavas@unic.cnrs-gif.fr>
2014-06-03 11:50:55 +0000

    Correct code for different injected currents using DCSource

--- I_f_curve.py
+++ I_f_curve.py
@@ -15,6 +15,9 @@ May 2006
 
 from pyNN.utility import get_script_args, normalized_filename
 
+N = 50
+max_current=3.0 #nA
+
 simulator_name = get_script_args(1)[0]  
 exec("import pyNN.%s as sim" % simulator_name)
 
@@ -25,13 +28,16 @@ ifcell  = sim.IF_cond_exp(
     tau_syn_E=5.0, tau_syn_I=5.0, v_reset=-70.0, e_rev_E=0., 
     e_rev_I=-100., v_rest=-50., tau_m=20.)
 
-popcell = sim.Population(2,ifcell)
+popcell = sim.Population(N,ifcell)
 
-current_source = sim.DCSource(amplitude=2.0)
-popcell.inject(current_source)
+for i in xrange(N):
+    current_source[i] = sim.DCSource(amplitude=max_current*i/N)
+    
+for i in xrange(N):
+    popcell[i:(i+1)].inject(current_source)
 
 filename = normalized_filename("Results", "IF_cond_exp", "pkl", simulator_name)
-sim.record(['v', 'gsyn_exc', 'gsyn_inh'], popcell, filename,
+sim.record(['spikes', 'v'], popcell, filename,
        annotations={'script_name': __file__})
 
 sim.run(200.0)
